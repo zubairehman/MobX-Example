32920ab158feed4c9ad4bdbad7780e2167f14358
zubairehman <zr@embrace-it.com>
2019-03-27 21:18:47 +0500

    - complete user info example using mobx and auto gen

--- lib/user_info/ui/user_info_page.dart
+++ lib/user_info/ui/user_info_page.dart
@@ -3,26 +3,33 @@ import 'package:mobx_example_app/user_info/model/user_info.dart';
 import 'package:mobx_example_app/user_info/ui/user_details_page.dart';
 import 'package:provider/provider.dart';
 
-class UserInfoPage extends StatelessWidget {
+class UserInfoPage extends StatefulWidget {
   UserInfoPage({Key key, this.title}) : super(key: key);
   final String title;
 
-  //text controllers
+  @override
+  _UserInfoPageState createState() => _UserInfoPageState();
+}
+
+class _UserInfoPageState extends State<UserInfoPage> {
+
   final TextEditingController _firstNameController = TextEditingController();
   final TextEditingController _lastNameController = TextEditingController();
   final TextEditingController _emailController = TextEditingController();
 
-  //form key
   final _formKey = GlobalKey<FormState>();
 
   @override
   Widget build(BuildContext context) {
+
+    print("rebuild gets called");
+
     //userInfo store
     final _userInfo = Provider.of<UserInfo>(context);
 
     return Scaffold(
       appBar: AppBar(
-        title: Text(title),
+        title: Text(widget.title),
       ),
       body: Form(
         key: _formKey,
@@ -59,28 +66,26 @@ class UserInfoPage extends StatelessWidget {
                   decoration: InputDecoration(hintText: 'Enter email'),
                 ),
                 Padding(padding: EdgeInsets.only(top: 30.0)),
-                Flexible(
-                  child: MaterialButton(
-                    color: Colors.lightBlue,
-                    onPressed: () {
-                      _userInfo.firstName = _firstNameController.text;
-                      _userInfo.lastName = _lastNameController.text;
-                      _userInfo.email = _emailController.text;
+                MaterialButton(
+                  color: Colors.lightBlue,
+                  onPressed: () {
+                    _userInfo.firstName = _firstNameController.text;
+                    _userInfo.lastName = _lastNameController.text;
+                    _userInfo.email = _emailController.text;
 
-                      //take user to next screen
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                            builder: (context) => UserDetailsPage()),
-                      );
-                    },
-                    child: Text(
-                      'Submit',
-                      style: Theme.of(context)
-                          .textTheme
-                          .button
-                          .copyWith(color: Colors.white),
-                    ),
+                    //take user to next screen
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                          builder: (context) => UserDetailsPage()),
+                    );
+                  },
+                  child: Text(
+                    'Submit',
+                    style: Theme.of(context)
+                        .textTheme
+                        .button
+                        .copyWith(color: Colors.white),
                   ),
                 )
               ],
